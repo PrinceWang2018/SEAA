fb1623a90d781f47242cb6715f209c2dc580ecc9
PrinceWang <37299182+PrinceWang2018@users.noreply.github.com>
2025-03-27 21:16:34 +0800

    Update hellomessage.R
    Information of publication

--- R/hellomessage.R
+++ R/hellomessage.R
@@ -6,7 +6,10 @@
 Project URL: https://github.com/PrinceWang2018/SEAA
 Reference URL: https://github.com/PrinceWang2018/SEAA_reference
 If you use it in published research, please cite:
-Wang et al., (2021). SEAA: Splicing Efficiency Analysis and Annotation (to be published)
+Qin, J., Huang, T., Wang, Z. et al. Bud31-mediated alternative splicing
+is required for spermatogonial stem cell self-renewal and differentiation. 
+Cell Death Differ (2022). https://doi.org/10.1038/s41418-022-01057-1
+If you have any question, please contact: wangzixiang@sdu.edu.cn
 =========================================================================================
                        --Tomorrow will be more beautiful! ^_^--")
   base::packageStartupMessage(msg)
